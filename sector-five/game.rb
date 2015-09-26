e1148fc02e31a20d67f40740783359e5b6b9f7c3
Anton Antonov <anton.synd.antonov@gmail.com>
2015-09-26 22:03:15 +0300

    Added movement, screen constraints and enemies

--- sector-five/game.rb
+++ sector-five/game.rb
@@ -1,6 +1,7 @@
 require 'gosu'
 
 require_relative 'player'
+require_relative 'enemy'
 
 class SectorFive < Gosu::Window
   SCREEN_WIDTH = 800
@@ -10,10 +11,21 @@ class SectorFive < Gosu::Window
     super(SCREEN_WIDTH, SCREEN_HEIGHT)
     self.caption = 'Sector Five'
     @player = Player.new(self)
+    @enemy =  Enemy.new(self)
   end
 
   def draw
     @player.draw
+    @enemy.draw
+  end
+
+  def update
+    @player.turn_left if button_down? Gosu::KbLeft
+    @player.turn_right if button_down? Gosu::KbRight
+    @player.accelerate if button_down? Gosu::KbUp
+    @player.move
+
+    @enemy.move
   end
 end
 
--- sector-five/player.rb
+++ sector-five/player.rb
@@ -1,12 +1,70 @@
 class Player
+  TURN_RATE = 4
+  ACCELERATION = 1
+  FRICTION = 0.8
+
   def initialize(window)
+    @image = Gosu::Image.new('images/ship.png')
+
     @x = 200
     @y = 200
     @angle = 0
-    @image = Gosu::Image.new('images/ship.png')
+
+    @velocity_x = 0
+    @velocity_y = 0
+
+    @radius = 20
+    @window = window
   end
 
   def draw
     @image.draw_rot(@x, @y, 1, @angle)
+  end
+
+  def turn_left
+    @angle -= TURN_RATE
+  end
+
+  def turn_right
+    @angle += TURN_RATE
+  end
+
+  def accelerate
+    @velocity_x += Gosu.offset_x(@angle, ACCELERATION)
+    @velocity_y += Gosu.offset_y(@angle, ACCELERATION)
+  end
+
+  def move
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= FRICTION
+    @velocity_y *= FRICTION
+
+    set_x_constraints
+    set_y_constraints
+  end
+
+  def set_x_constraints
+    if @x > @window.width - @radius
+      @velocity_x = 0
+      @x = @window.width - @radius
+    end
+
+    if @x < @radius
+      @velocity_x = 0
+      @x = @radius
+    end
+  end
+
+  def set_y_constraints
+    if @y > @window.height - @radius
+      @velocity_y = 0
+      @y = @window.height - @radius
+    end
+
+    if @y < @radius
+      @velocity_y = 0
+      @y = @radius
+    end
   end
 end
